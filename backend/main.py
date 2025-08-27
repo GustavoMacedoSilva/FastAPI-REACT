e87745b4f130eb98d11e620adc38798a32147344
GustavoMSilva <macedo.gustavo@ges.inatel.br>
2025-08-27 14:28:04 -0300

    Alguns tipos de Query na main(Optional, e default)

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 # uvicorn main:app --reload
-@app.get('/')
-def index():
-    return {'data': {'name': 'Gugu'}}
+@app.get('/artist')#apenas pegamos 10 artistas
+def index(limit =10,most_famous:bool=False, sort: Optional[str]= None):
+    if most_famous:
+        return {'data': f'{limit} most famous artist from the db'}
+    else:
+        return {'data': f'{limit} artist from the db'}
 
-@app.get('/about')
-def about():
-    return {'data': 'About Page'}
+@app.get('/artist/{artist_id}')
+def showArtist(artist_id: int):
+    return {'data': artist_id}
+
+@app.get('/user/{user_username}/listenedAlbums')
+def listenedAlbums(user_username: str):
+    return {'data': {'Album 1', 'Album 2'}}
+
+@app.get('/user/{user_username}/reviews')
+def userReviews(user_username):
+    return {'data': {'review1': "blablabla", 'review2': "blablablba"}}
 
 
 if __name__ == "__main__":
